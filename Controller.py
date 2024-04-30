f9b5bf15c2aa460e89e6476be6d26620a6671846
EddySigma <j.eddysigma@gmail.com>
2024-04-30 16:52:22 -0700

    Minor readability changes

--- Controller.py
+++ Controller.py
@@ -1,25 +1,25 @@
 import pygame
 from dataclasses import dataclass
 
+
 @dataclass
 class Thing:
     image: pygame.surface.Surface
     x_pos: int
     y_pos: int
 
+
 class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-
         self.running = True
-        
-    
+
     def run(self):
         keys_pressed = pygame.key.get_pressed()
         self.handle_user_events()
         self.handle_user_input(keys_pressed)
-        self.display_to_screen()
+        self.send_items_to_display()
         return self.running
 
     def handle_user_events(self):
@@ -32,35 +32,47 @@ class Controller:
     # a little more space to work with.
 
     def handle_user_input(self, keys_pressed):
-        if (keys_pressed[pygame.K_w]
-            and self.model.player.y_pos >= (0 - self.model.player.height) * 0.25):
+        if (
+            keys_pressed[pygame.K_w]
+            and self.model.player.y_pos >= (0 - self.model.player.height) * 0.25
+        ):
             self.model.player.move_forward()
 
-        if (keys_pressed[pygame.K_s]
-            and self.model.player.y_pos <= self.view.PLAY_AREA_HEIGHT - (self.model.player.height * 0.75)):
+        if keys_pressed[
+            pygame.K_s
+        ] and self.model.player.y_pos <= self.view.PLAY_AREA_HEIGHT - (
+            self.model.player.height * 0.75
+        ):
             self.model.player.move_back()
 
-        if (keys_pressed[pygame.K_a]
-            and self.model.player.x_pos >= (0 - self.model.player.width) * 0.25):
+        if (
+            keys_pressed[pygame.K_a]
+            and self.model.player.x_pos >= (0 - self.model.player.width) * 0.25
+        ):
             self.model.player.move_left()
 
-        if (keys_pressed[pygame.K_d] 
-            and self.model.player.x_pos <= self.view.PLAY_AREA_WIDTH - (
-            self.model.player.width * 0.75)):
+        if keys_pressed[
+            pygame.K_d
+        ] and self.model.player.x_pos <= self.view.PLAY_AREA_WIDTH - (
+            self.model.player.width * 0.75
+        ):
             self.model.player.move_right()
-        
 
-    def display_to_screen(self):
-        paiload = []
-        paiload.append(Thing(self.model.player.ship, self.model.player.x_pos, self.model.player.y_pos))
-        
+    def send_items_to_display(self):
+        display_paiload = []
+        display_paiload.append(
+            Thing(
+                self.model.player.ship, self.model.player.x_pos, self.model.player.y_pos
+            )
+        )
+
         for item in self.model.player_attacks:
-            paiload.append(Thing(item.image, item.x_pos, item.y_pos))
+            display_paiload.append(Thing(item.image, item.x_pos, item.y_pos))
 
         for item in self.model.enemies:
-            paiload.append(Thing(item.image, item.x_pos, item.y_pos))
-        
+            display_paiload.append(Thing(item.image, item.x_pos, item.y_pos))
+
         for item in self.model.enemy_attacks:
             plaiload.append(Thing(item.sprite, item.x_pos, item.y_pos))
 
-        self.view.add_to_display_queue(paiload)
+        self.view.add_to_display_queue(display_paiload)
